a3c7f2a892927b49cf48d2a50c0206d36b3154f2
lilyhuang <lily.huang@coupang.com>
2018-01-18 14:38:07 +0800

    init
    init

---
+++ src/main/java/com/lily/study/TopN.java
@@ -0,0 +1,18 @@
+package com.lily.study;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+
+public class TopN {
+
+
+    public static void main(String[] args){
+
+
+        SparkConf sparkConf=new SparkConf().setAppName("topN");
+
+        SparkContext sparkContext=new SparkContext(sparkConf);
+
+        sparkContext.
+    }
+}
